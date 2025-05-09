db39b3d9e4f81ca50a0d9b7e9ce1919cb7b08e0a
Archit Anirudhan <architanirudhan123@gmail.com>
2025-05-09 17:49:30 +0530

    created pages added some styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
     <>
-        <div className='flex justify-between items-center bg-black p-4'>
-          <p className='text-lg text-orange-400 font-mono font-bold tracking-wide'>KARTO</p>
-          <form className='flex items-center gap-2'>
-            <input type="search" placeholder="Search for Products" className='text-orange-200 bg-black border-2 border-orange-400 focus:outline-2 focus:outline-offset-2 focus:outline-orange-400 px-2 rounded-sm'/>
-            <button type="submit" className='bg-orange-400 text-black px-4  rounded-sm hover:bg-orange-500 transition duration-300'>Search</button>
-          </form>
-        </div>
+      <div className="flex justify-between items-center bg-black p-4 fixed top-0 left-0 right-0 z-10">
+        <p className="text-xl text-orange-400 font-mono font-bold tracking-wide hover:text-orange-500 transition duration-300">
+          KARTO
+        </p>
+        <form className="flex items-center gap-2">
+          <input
+            type="search"
+            placeholder="Search for Products"
+            className="text-orange-200 bg-black border-2 border-orange-400 focus:outline-2 focus:outline-offset-2 focus:outline-orange-400 px-2 rounded-sm"
+          />
+          <button
+            type="submit"
+            className="bg-orange-400 text-black px-4 py-1 rounded-sm hover:bg-orange-500 transition duration-300 "
+          >
+            Search
+          </button>
+        </form>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Header from './components/Header'
+import ProductsPage from './pages/Productspage'
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
+    <ProductsPage />
   </StrictMode>,
 )
