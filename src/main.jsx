31d0371ff79d48028dd08bc4017a7db35304bcbb
Archit Anirudhan <architanirudhan123@gmail.com>
2025-05-11 17:11:06 +0530

    renamed products page

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import ProductsPage from './pages/Productspage'
+import ProductsPage from './pages/Products-page'
 
 
 
